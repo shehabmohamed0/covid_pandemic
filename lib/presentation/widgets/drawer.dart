8223367e733c8ba6143845abbf58e5db1b966149
Shehab <shehabmohamed415@gmail.com>
2021-10-03 09:03:34 +0200

    final Project

--- lib/presentation/widgets/drawer.dart
+++ lib/presentation/widgets/drawer.dart
@@ -1,5 +1,6 @@
 import 'package:covid_pandemic/core/constants/palette.dart';
 import 'package:covid_pandemic/logic/cubit/authentication/authentication_cubit.dart';
+import 'package:covid_pandemic/presentation/screens/questions/widgets/web_view.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -19,6 +20,46 @@ class DrawerWidget extends StatelessWidget {
           children: [
             const SizedBox(
               height: 40,
+            ),
+            Container(
+              color: Colors.transparent,
+              child: ListTile(
+                leading: const Icon(
+                  Icons.web,
+                  color: Colors.white,
+                ),
+                title: const Text(
+                  'Ministry of Health',
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const WebView2(
+                            url: 'https://www.hhs.gov/',
+                          )));
+                },
+              ),
+            ),
+            Container(
+              color: Colors.transparent,
+              child: ListTile(
+                leading: const Icon(
+                  Icons.web_asset_rounded,
+                  color: Colors.white,
+                ),
+                title: const Text(
+                  'Vaccine locations near you',
+                  style: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const WebView2(
+                            url: 'https://www.vaccines.gov/',
+                          )));
+                },
+              ),
             ),
             Container(
               color: Colors.transparent,
